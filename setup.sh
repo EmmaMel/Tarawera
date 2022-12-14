bd15a0fae97aa662ba3ddbbe0efec33905f81274
EmmaMel <90302400+EmmaMel@users.noreply.github.com>
2022-12-15 11:49:10 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,18 +10,11 @@ fi
 
 echo "-------------------------------------------------------------------------------"
 echo "Welcome to the Tarawera installation."
-echo "Please ensure system is up-to-date and meets the following prerequisites:"
+#echo "Please ensure system is up-to-date and meets the following prerequisites:"
 echo ""
-echo "If installing on a Unix host, ensure you have the following packages installed: "
-echo "[*]  Docker version 20.10.17 or higher"
-echo "[*]  git version 2.31.1 or higher"
-echo ""
-echo "If installing on a Windows host, ensure you have the following packages installed: "
-echo "[*]  WSL 2.0 or higher"
-echo "[*]  git version 2.31.1 or higher"
-echo "[*]  Docker installed to WSL 2.0"
-echo "[*]  WSL 2.0 Ubuntu 20.04 profile"
-echo "[*]  git version 2.30 or higher on Ubuntu profile"
+#echo "If installing on a Windows host, ensure you have the following packages installed: "
+#echo "[*]  WSL 2.0 or higher"
+#echo "[*]  WSL 2.0 Ubuntu 20.04 profile or higher"
 echo "-------------------------------------------------------------------------------"
 
 while true; do
@@ -33,7 +26,9 @@ while true; do
     esac
 done
 
-read -p 'Docker Container name: ' containernamevar
+
+apt-get update && apt-get -y install python3 python3-pip git curl zip unzip upx golang haproxy zlib1g -y
+
 echo "Red Team Password: "
 read -s redpasswordvar
 echo "Blue Team Password: "
@@ -43,66 +38,32 @@ echo "Tarawera installing.......";
 echo "-------------------------------------------------------------------------------"
 
 
-# Check for docker, progress if located otherwise exit
-if ! docker version ; then
-   #echo "Docker not found. Please install Docker and try again"
-  echo "Docker not found. Installing Docker....."
-  snap install docker
-  #exit 1
-
-elif docker version ; then
-  echo "Docker Found, Continuing"
-fi
-
-mkdir TaraweraCaldera
-cd TaraweraCaldera
+git clone https://github.com/EmmaMel/Tarawera.git --recursive --branch Automate
+cd Tarawera
 git clone https://github.com/mitre/caldera.git --recursive --branch 4.1.0
 cd caldera
-rm Dockerfile
+rm requirements.txt
+cd ../
+cp requirements.txt caldera/
+cd caldera
+pip3 install -r requirements.txt 
+cd plugins/emu
+./download_payloads.sh
+cd ../
+cd sandcat
+./update-agents.sh
 cd ../..
-cp Dockerfile TaraweraCaldera/caldera/
-cd TaraweraCaldera/caldera
 
-# Update passwords
 sed -i '/^users\:/q' conf/default.yml
 sed -i "/users\:/ s/.*/users\:\n  blue\:\n    blue\: ${bluepasswordvar}\n  red\:\n    red\: ${redpasswordvar}/" conf/default.yml
 
-cd plugins/sandcat
-./update-agents.sh
-
-cd ../
-cd emu
-./download_payloads.sh
-
-cd ../..
-
-# Pull Caldera Docker Image
-# if docker pull mitre/caldera ; then
-#   echo "Docker Caldera Image pull successful"
-# else
-#   echo "Error. Failed pulling mitre/calder image from DockerHub. Please manually try to pull."
-#   exit 1
-# fi
-
-# Build and run Docker Image
-#docker build . --build-arg WIN_BUILD=true -t mitre/caldera
-#sudo docker run -it -d --name ${containernamevar} --hostname ${containernamevar} -p 7010:7010 -p 7011:7011 -p 7012:7012 -p 8888:8888 mitre/caldera
-docker build . --build-arg WIN_BUILD=true -t caldera:latest
-#docker run -it -d --name ${containernamevar} --hostname ${containernamevar} -p 7010:7010 -p 7011:7011 -p 7012:7012 -p 8888:8888 caldera:latest
-docker run -it -d --name ${containernamevar} --hostname ${containernamevar} --volume conf:/usr/src/app/conf -p 7010:7010 -p 7011:7011 -p 7012:7012 -p 8888:8888 caldera:latest
-sleep 10
-
-# Update and restart Docker
-docker exec ${containernamevar} bash -c "apt-get update"
-docker exec ${containernamevar} bash -c "apt-get upgrade -y"
-docker exec -it ${containernamevar} bash -c "apt-get install upx -y"
-docker update --restart=always ${containernamevar}
-sleep 5
-docker container restart ${containernamevar}
-sleep 5
 
 echo "-------------------------------------------------------------------------------"
 echo "Tarawera installation complete!.......";
 echo "Navigate to http://localhost:8888/ to access the web console."
 echo "Login details can be found in mitreCaldera/caldera/conf/default.yml"
 echo "-------------------------------------------------------------------------------"
+
+python3 server.py
+
+
