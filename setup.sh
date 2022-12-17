b0e707144a0fd64f30794ef28be2ced16e20396d
EmmaMel <90302400+EmmaMel@users.noreply.github.com>
2022-12-17 22:11:07 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,7 +27,7 @@ while true; do
 done
 
 
-DEBIAN_FRONTEND=noninteractive apt-get update && apt-get -y install python3 python3-pip git curl zip unzip upx golang haproxy zlib1g gnome-terminal docker -y
+DEBIAN_FRONTEND=noninteractive apt-get update && apt-get -y install python3 python3-pip git curl zip unzip upx golang haproxy zlib1g -y
 
 echo "Red Team Password: "
 read -s redpasswordvar
@@ -77,7 +77,7 @@ usermod $USER -a -G docker
 apt-get remove -y docker docker-engine docker.io containerd runc
 apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable'
+add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' -y
 apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
 
 
